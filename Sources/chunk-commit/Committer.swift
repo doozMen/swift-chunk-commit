dd76d10974b61b8ef767da48362ea6cc406ea5d9
stijn <stijn.willems@mediahuis.com>
2024-10-22 15:53:56 +0200

    fix executable

--- Sources/chunk-commit/Committer.swift
+++ Sources/chunk-commit/Committer.swift
@@ -1,8 +1,9 @@
 import ArgumentParser
+import ChunkCommiter
 import Foundation
 
 @main
-struct Committer: AsyncParsableCommand {
+struct ChunkCommit: AsyncParsableCommand {
   static let configuration = CommandConfiguration(
     version: "0.0.1",
     subcommands: [Chunk.self, SetupTest.self]
